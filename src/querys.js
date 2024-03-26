a3cd09e4f878f4555250c7bdec62c904fe7288b3
lndsilva <lndsilva@hotmail.com>
2024-03-26 11:51:55 -0300

    criando CRUD backend nodejs

--- src/querys.js
+++ src/querys.js
@@ -10,4 +10,29 @@ const getActorsById = async (id) => {
     return query
 }
 
-module.exports = { getAllActors, getActorsById }
+const createActor = async (first_name, last_name) =>{
+    const [query] = await connection.execute(`INSERT INTO sakila.actor (first_name, last_name) VALUES (?,?)`, [first_name,last_name])
+    const item = await getActorsById(query.insertId)
+    return item
+}
+
+const updateActor = async (id, first_name, last_name) =>{
+    const item = await getActorsById(id)
+    if(item.length === 0 ){
+        return null 
+    }
+    const [query] = await connection.execute(`UPDATE sakila.actor 
+    SET first_name = ?, last_name = ? WHERE actor_id = ?;`, [first_name, last_name, id])
+    return query
+}
+const deleteActor = async (id) => {
+    const item = await getActorsById(id)
+    if(item.length === 0 ){
+        return null
+    }
+    const [query] = await connection.execute(`DELETE FROM sakila.actor WHERE actor_id = ?;`, [id])
+    
+    return query
+}
+
+module.exports = { getAllActors, getActorsById, createActor, updateActor }
--- src/routes/actors.js
+++ src/routes/actors.js
@@ -19,4 +19,28 @@ router.get('/:id', async (req, res)=>{
     return res.status(200).json(query)
 })
 
+router.post('/', async (req, res)=>{
+    const { first_name, last_name } = req.body
+    const query = await querys.createActor(first_name,last_name)
+    return res.status(200).json(query)
+})
+
+router.put('/', async (req, res)=>{
+    const { id, first_name, last_name } = req.body
+    const query = await querys.updateActor(id, first_name, last_name)
+    if(query === null){
+        return res.status(400).json({message : 'Ator não encontrado'})
+    }
+    return res.status(200).json({message: 'Ator registrado com sucesso!!!'})
+})
+
+router.delete('/', async (req, res) =>{
+    const { id } = req.body
+    const query = await querys.deleteActor(id)
+    if(query === 0 ){
+        return res.status(400).json({message: 'Ator não encontrado.'})
+    }
+    return res.status(200).json({message: 'Ator deletado com sucesso!!!'})
+})
+
 module.exports = router
